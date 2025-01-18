fbcf17b849df19fdb6fe5e4ac24802639ae3406d
MOORO <fedorovmatvey48@gmail.com>
2025-01-18 13:39:57 +0300

    Изменена структура файлов, готова заставка, но без текста

---
+++ config.py
@@ -0,0 +1,14 @@
+FPS = 60
+WIDTH = 500
+HEIGHT = 500
+
+BACKGROUND_IMAGE = 'image/background_img.jpg'
+BULLET_IMAGE = 'image/bullet.png'
+
+PLAYER_DOWN = 'image/move_down.png'
+PLAYER_UP = 'image/move_up.png'
+PLAYER_LEFT = 'image/move_left.png'
+PLAYER_RIGHT = 'image/move_right.png'
+
+BD_FILE = 'data/game_db'
+FONT_FILE = 'data/font.ttf'
--- main.py
+++ main.py
@@ -1,20 +1,72 @@
 import pygame
 import sys
 import sqlite3
+import os
+
+import config
+
+FPS = config.FPS
+clock = pygame.time.Clock()
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert()
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image(config.BACKGROUND_IMAGE), (config.WIDTH, config.HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(config.FONT_FILE, 15)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(config.FPS)
+
+
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Движущийся круг 2')
-    size = width, height = 800, 400
+    pygame.display.set_caption('GAME')
+    size = width, height = config.WIDTH, config.HEIGHT
     screen = pygame.display.set_mode(size)
+    start_screen()
 
     running = True
-    FPS = 60
-    clock = pygame.time.Clock()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        clock.tick(FPS)
+        clock.tick(config.FPS)
         pygame.display.flip()
     pygame.quit()
